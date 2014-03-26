bb5b629816512225a8a0a83002140e769cb81399
Paras Kuhad <paras.kuhad@gmail.com>
2014-03-26 13:33:13 +0530

    table scroll row select callback function assigned

--- js/table.js
+++ js/table.js
@@ -1,7 +1,10 @@
 angular.module('helloApp')
 
 .controller('TableCtrl', ['$scope', function($scope){
-	$scope.last_key_code = null;
+	$scope.take_action = function(row_args){
+		console.log(row_args);
+		alert('hi');
+	}
 }])
 
 .directive('scrollTable', ['$q', function($q){
@@ -10,7 +13,9 @@ angular.module('helloApp')
 		// name: '',
 		// priority: 1,
 		// terminal: true,
-		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		scope: {
+			scrollTableCallback: '&',
+		}, // {} = isolate, true = child, false/undefined = no change
 		// controller: function($scope, $element, $attrs, $transclude) {},
 		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
@@ -20,22 +25,45 @@ angular.module('helloApp')
 		// transclude: true,
 		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 		link: function(scope, element, attrs) {
+			var ROW_START_INDEX = 0;
 			var table = {
 				'row_max': 0,
-				'selected_row': 0,
+				'selected_row': ROW_START_INDEX,
+				'row_elements': element.find('tbody tr'),
 			}
-			table.row_max = element.find('tbody tr').length;
 
+			//Init
+			table.row_max = element.find('tbody tr').length;
+			table.row_elements.eq(table.selected_row).addClass('selected-row');
+			table.row_args = table.row_elements.eq(table.selected_row).data('row-args');
 			element[0].focus();
+
+
+			//Event Handlers
 			element.on('keydown', function(e){
+				if (e.keyCode == 40){
+					if (table.selected_row != table.row_max-1){
+						table.row_elements.eq(table.selected_row).removeClass('selected-row');
+						table.selected_row += 1;
+						table.row_args = table.row_elements.eq(table.selected_row).data('row-args');
+						table.row_elements.eq(table.selected_row).addClass('selected-row');
+					}
+				}
+				else if (e.keyCode == 38){
+					if (table.selected_row !=ROW_START_INDEX){
+						table.row_elements.eq(table.selected_row).removeClass('selected-row');
+						table.selected_row -= 1;
+						table.row_args = table.row_elements.eq(table.selected_row).data('row-args');
+						table.row_elements.eq(table.selected_row).addClass('selected-row');
+					}
+				}
+				else if (e.keyCode == 13){
+					console.log(table.row_args);
+					scope.scrollTableCallback({row_args: table.row_args});
+				}
 				console.log(e.keyCode);
-				scope.last_key_code = e.keyCode;
-				scope.$apply();
 			});
 			console.log(element.find('tbody tr').length);
-			console.log(element.find('tbody tr'));
-			console.log('hello world');
-			console.log(element[0]);
 		}
 	};
 }]);
